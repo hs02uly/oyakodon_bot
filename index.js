e54fffa9295175c2cdd97ef992dbdeffb37e9617
hs02uly <hs02uly@gmail.com>
2022-12-03 07:25:53 +0900

    Fix

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ http.createServer(function (req, res) {
     res.end()
 }).listen(8080)
 
-// const { Client, EmbedBuilder, GatewayIntentBits, Partials } = require("discord.js");
-// const { Guilds, GuildMessages, MessageContent } = GatewayIntentBits;
-// const client = new Client({
-//     "intents": [Guilds, GuildMessages, MessageContent],
-//     "partials": [Partials.Channel]
-// });
+const { Client, EmbedBuilder, GatewayIntentBits, Partials } = require("discord.js");
+const { Guilds, GuildMessages, MessageContent } = GatewayIntentBits;
+const client = new Client({
+    "intents": [Guilds, GuildMessages, MessageContent],
+    "partials": [Partials.Channel]
+});
 
 client.once("ready", () => {
     console.log(`起動しましたよ覚悟しなさい${new Date()}`)
@@ -21,38 +21,38 @@ const c = "#73efff"
 let args = []
 client.on("messageCreate", async message => {
     try{
-
-        //                  oyakodon                    //
-        if (!message.content.startsWith(p)){
-            if(message.content.includes("youtube" || "YouTube" || "ニコ動")) message.reply("投稿者としての自覚はないんか？")
-            if(message.content.includes("彼女" || "食べ")) message.reply("よかったら僕を食べませんか")
-            if(message.content.includes("彼女" || "食べ")) message.reply("")
-            if(message.mentions.users.has(client.user.id)) message.reply("メンションしないでください\n禿げさせますよ")
-        }
         const p = "o."
         const cmd = message.content.slice(2).split(" ")[0]
         args = message.content.split(" ").slice(1)
 
-        if (!message.content.startsWith(p)) return;
         if (message.author.bot) return;
-        console.log("認識された引数:",args)
+        //                  oyakodon                    //
+        if (!message.content.startsWith(p)){
+            if(message.content.includes("youtube" || "YouTube" || "ニコ動")) return message.reply("投稿者としての自覚はないんか？");
+            if(message.content.includes("彼女" || "食べ")) return message.reply("よかったら僕を食べませんか");
+            if(message.content.includes("解決しな")) return message.reply("問題が解決しなかったら製作者を交換すればいいですね");
+            if(message.mentions.users.has(client.user.id)) return message.reply("メンションしないでください\n禿げさせますよ");
+        }
+
+        if (!message.content.startsWith(p)) return;
+        console.log("認識された引数:",args);
 
         //                  commands                    //
         switch(cmd){
             case "say":
-                return message.channel.send(args.join(" "))
+                return message.channel.send(args.join(" "));
                 break;
 
-            case `ping`:
+            case "ping":
                 const ping = new EmbedBuilder()
                     .setTitle("Pong")
                     .addFields(
                         { name: "WebSocket", value: `${client.ws.ping}ms`, inline: true},
-                        { name: "コマンド受信", value: `${new Date()() - message.createdTimestamp}ms`, inline: true}
+                        { name: "コマンド受信", value: `${new Date() - message.createdTimestamp}ms`, inline: true}
                     )
                     .setColor(c)
-                    .setTimestamp()
-                return message.reply({embeds: [ping]})
+                    .setTimestamp();
+                return message.reply({embeds: [ping]});
                 break;
 
             case "help":
@@ -66,14 +66,14 @@ client.on("messageCreate", async message => {
                     )
                     .setColor(c)
                     .setTimestamp()
-                return message.reply({embeds: [help]})
+                return message.reply({embeds: [help]});
                 break;
             case "test":
-                console.log("反応あり")
+                console.log("反応あり");
                 break;
 
             case "okd":
-                let d = new Date()
+                let d = new Date();
                 let oyakodonM = [
                     "息の根が終了しました",
                     "あ、勝手に慈悲受け取るマンです",
@@ -96,25 +96,25 @@ client.on("messageCreate", async message => {
                     "親子丼Botが可哀想....."
                     // `${d.getMinutes()}:${d.getMinutes()}になりました\n約束のブツを出してください`
                     // `親子丼Botが${d.getHours()}時${d.getMinutes()}分をお知らせします`
-                ]
+                ];
                 switch (args[0]) {
                     case "list":
                         const okd = new EmbedBuilder()
                             .setColor(c)
-                            .addFields({ name: "一覧", value: oyakodonM.join("\n") })
-                        return message.reply({embeds: [okd]})
+                            .addFields({ name: "一覧", value: oyakodonM.join("\n") });
+                        return message.reply({embeds: [okd]});
                         break;
                     default:
-                        return message.reply(oyakodonM[Math.floor(Math.random() * oyakodonM.length)])
+                        return message.reply(oyakodonM[Math.floor(Math.random() * oyakodonM.length)]);
                         break;
                 }
 
                 default:
-                    return message.reply("コマンドが不明です")
+                    return message.reply("コマンドが不明です");
                     break;
         }
     }catch(e){
-        return message.reply(e.message)
+        return message.reply(e.message);
     }
 })
 
