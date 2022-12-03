18ae285daa3704a60bb231c3da5602177721fc4c
hs02uly <hs02uly@gmail.com>
2022-12-04 08:10:10 +0900

    updata 動くかな？

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//もしかしてmiyuさんだったりします？
+//公開しないでぴえん
+
 const http = require("http")
 http.createServer(function (req, res) {
     res.write("online")
@@ -28,19 +31,25 @@ client.on("messageCreate", async message => {
         if (message.author.bot) return;
         //                  oyakodon                    //
         if (!message.content.startsWith(p)){
+            if(message.content.includes("|​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​|")) return message.reply("私には通用しませんよ");
             if(message.content.includes("youtube" || "YouTube" || "ニコ動")) return message.reply("投稿者としての自覚はないんか？");
             if(message.content.includes("彼女" || "食べ")) return message.reply("よかったら僕を食べませんか");
             if(message.content.includes("解決しな")) return message.reply("問題が解決しなかったら製作者を交換すればいいですね");
+            if(message.content.includes("🤔🤔")) return message.reply("出たな！妖怪シンキングマン");
             if(message.mentions.users.has(client.user.id)) return message.reply("メンションしないでください\n禿げさせますよ");
         }
 
         if (!message.content.startsWith(p)) return;
-        console.log("認識された引数:",args);
+        console.log("認識されたコマンド:", message.content);
 
         //                  commands                    //
         switch(cmd){
             case "say":
-                return message.channel.send(args.join(" "));
+                if (!args[0] && !message.attachment.size) return; //文字なし&ファイルの添付なしの時無視
+                if (message.attachment.size){
+                    const files = message.attachment
+                    return (!args[0] ? message.channel.send({ files }) :message.channel.send({ content: args.join(" "), files: [files] }))
+                }else return message.channel.send(args.join(" "));
                 break;
 
             case "ping":
@@ -60,7 +69,7 @@ client.on("messageCreate", async message => {
                     .setTitle("Help/commands")
                     .setDescription("親子丼氏の許可のもと作成しています")
                     .addFields(
-                        { name: "okd", value: `ランダムで親子丼氏の名言を送信します\n名言募集中です`, inline: true},
+                        { name: "okd", value: `ランダムで親子丼氏の名言を送信します。名言募集中です\n引数: list(listを表示します), \nなんか数字(その数字番目のokd名言を送信します`, inline: true},
                         { name: "say", value: `botになにか言わせれます\nなぜかスペースに対応していません`, inline: true},
                         { name: "ping", value: "ping値を測ります", inline: true}
                     )
@@ -68,6 +77,16 @@ client.on("messageCreate", async message => {
                     .setTimestamp()
                 return message.reply({embeds: [help]});
                 break;
+
+            case "sayc":
+                if(message.author.id == "888652878590406656"){
+                    return client.channels.cache.get(args[0]).send(args[1])
+
+                }else{
+                    return message.reply("あなたにその権限はありません")
+                }
+                break;
+
             case "test":
                 console.log("反応あり");
                 break;
@@ -88,30 +107,43 @@ client.on("messageCreate", async message => {
                     "勝者が決定しました！",
                     "もしも僕がサブアカウントを秘密裏に運営していると言ったらどうします？",
                     "許さない",
+                    "許します",
                     "こたつになりなさい",
                     "棺桶入ります？",
+                    "可哀想.....あなたに心は無いんですか！",
                     "管理者権限ください",
                     "そのチョコ点ってたっぷりよな、トッポまですげぇ最後だもん。",
                     "理解しました\nメンションされてませんが禿げさせます",
-                    "親子丼Botが可哀想....."
+                    "親子丼Botが可哀想.....",
+                    "全員が心にチャーシューを飼っているので全員チャーシューです",
+                    ":partying_face:",
+                    "シールさんも食べ物でしたね",
+                    "完全にマンボウになるまでマンボウもどきと呼びます",
+                    "黙れマンボウもどき",
+                    "すみません",
+                    "現在進行形で社畜を募集しています",
+                    "分かりました、マンボウもどき",
+                    "素晴らしい人材だ.....！\nあなたも社畜になりませんか？",
+                    "通常は時給-1000円程度の賠償が発生しますが、社畜になると免除されます！",
+                    "皆さんが油断している隙に防城戦ワールドを開きます",
                     // `${d.getMinutes()}:${d.getMinutes()}になりました\n約束のブツを出してください`
                     // `親子丼Botが${d.getHours()}時${d.getMinutes()}分をお知らせします`
                 ];
-                switch (args[0]) {
-                    case "list":
-                        const okd = new EmbedBuilder()
-                            .setColor(c)
-                            .addFields({ name: "一覧", value: oyakodonM.join("\n") });
-                        return message.reply({embeds: [okd]});
-                        break;
-                    default:
-                        return message.reply(oyakodonM[Math.floor(Math.random() * oyakodonM.length)]);
-                        break;
+                if(args[0] === "list"){
+                    const okd = new EmbedBuilder()
+                        .setColor(c)
+                        .addFields({ name: "一覧", value: oyakodonM.join("\n") });
+                    return message.reply({embeds: [okd]});
                 }
-
-                default:
-                    return message.reply("コマンドが不明です");
-                    break;
+                if(!isNaN(args[0])){
+                    if (Number(args[0])-1 > oyakodonM.length || Number(args[0])-1 < 0) return;
+                    return message.reply(oyakodonM[Number(args[0])-1])
+                }else{
+                    return message.reply(oyakodonM[Math.floor(Math.random() * oyakodonM.length)]);
+                }
+            default:
+                return message.reply("コマンドが不明です");
+                break;
         }
     }catch(e){
         return message.reply(e.message);
